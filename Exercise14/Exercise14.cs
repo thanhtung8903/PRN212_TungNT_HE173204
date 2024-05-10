4d06aa56e6c19ce2bf1573751c6ffa954df6fa13
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 11:14:44 +0700

    Exercise 14

---
+++ Exercise14/Exercise14.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise14;
+
+public class Exercise14
+{
+    // This is the main method where the program execution starts
+    public static void Main( )
+    {
+        Console.Write("Enter the amount of Celsius: "); 
+        int celsius = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Kelvin = {0}", celsius + 273);
+
+        Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32);
+    }
+}
