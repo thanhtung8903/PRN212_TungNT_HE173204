236c0e0e028b802a278fccf76a8de166e1ec27ca
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 10:14:09 +0700

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello");
+
+            System.Console.WriteLine("Nguyen Thanh Tung HE173204!");
+        }
+    }
+}
