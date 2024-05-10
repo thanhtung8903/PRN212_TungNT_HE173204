9d40210f9738d72569a717496af655fcec484f79
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 11:36:36 +0700

    Exercise 20

---
+++ Exercise20/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise20;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter num1: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter num2: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Abs: {result(num1, num2)}");
+    }
+
+    public static int result(int a, int b)
+    {
+        if (a > b)
+        {
+            return (a - b) * 2;
+        } else
+        {
+            return b - a;
+        }
+    }
+}
