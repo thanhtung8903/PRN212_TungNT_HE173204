58b6d994499cf9ba7f4830caef7ea408c4f4ce92
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 11:12:26 +0700

    Exercise 13

---
+++ Exercise13/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 3; j++)
+            {
+                if ((i == 1 || i == 5) || (j == 1 || j == 3))
+                {
+                    Console.Write(num);
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine(); // Move this outside of the inner loop to print new line after each row
+        }
+    }
+}
