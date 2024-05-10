e95b6af06e9a1934eb80e35f3541923a174a4f87
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 11:32:58 +0700

    Exercise 19

---
+++ Exercise19/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise19;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter num1: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter num2: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int sum = num1 + num2;
+        if (num1 == num2)
+        {
+            sum = num1 * 3;
+        }
+
+        Console.WriteLine("Sum: " + sum);
+    }
+}
