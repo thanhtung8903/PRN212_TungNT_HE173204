87fa56e2889687676e9b5490693eae47950c55aa
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 11:17:17 +0700

    Exercise 15

---
+++ Exercise15/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise15;
+
+public class Exercise15 {
+    static void Main(string[] args)
+    {
+        Console.WriteLine(remove_char("w3resource", 1));
+        Console.WriteLine(remove_char("w3resource", 9)); 
+        Console.WriteLine(remove_char("w3resource", 0)); 
+    }
+
+    public static string remove_char(string str, int n)
+    {
+        return str.Remove(n, 1); 
+    }
+}
