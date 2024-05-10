62c38988446ad9ce3a66d9b274a2670c72c7bbd5
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 10:28:53 +0700

    Exercise 3

---
+++ Exercise3/Exercise3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    public class Exercise3
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("result of dividing two numbers: ");
+            Console.WriteLine($"{36} / {6} = {36 / 6}");
+        }
+    }
+}
