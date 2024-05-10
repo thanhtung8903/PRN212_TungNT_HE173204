3713ff84f5308e7d451fa056b58a2fc03cf332e0
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 11:59:30 +0700

    Exercise 25

---
+++ Exercise25/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Channels;
+
+namespace Exercise25;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 99; i++)
+        {
+            if (i % 2 == 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        
+    }
+}
