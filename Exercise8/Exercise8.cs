d6e9e76a33858921ba5c291612df53a40e7914dd
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 10:43:11 +0700

    Exercise 8

---
+++ Exercise8/Exercise8.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise8
+{
+    public class Exercise8
+    {
+        public static void Main()
+        {
+            int x; 
+            int result;
+
+            Console.WriteLine("Enter a number:"); 
+            x = Convert.ToInt32(Console.ReadLine()); 
+
+            for (int i = 1; i <= 10; i++)
+            {
+                   result = x * i; 
+                Console.WriteLine("{0} x {1} = {2}", x, i, result);
+            }
+
+        }
+    }
+}
