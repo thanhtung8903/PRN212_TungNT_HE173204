03706ee7f498700cc89be243338c46c0deb39e87
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 11:28:45 +0700

    Exercise 17

---
+++ Exercise17/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise17;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        Console.WriteLine(newString(str));
+    }
+
+    public static string newString(string str)
+    {
+        return str.Substring(0, 1) + str + str.Substring(0, 1);
+    }
+}
