a39c37cbe0c0ef485ac134b29b321be6829be789
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 11:57:13 +0700

    Exercise 24

---
+++ Exercise24/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise24;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        
+        string[] words = str.Split(new[] { " " }, StringSplitOptions.None);
+
+        string word = "";
+        int max = 0;
+
+        foreach (String s in words)
+        {
+            if (s.Length > max)
+            {
+                word = s; 
+                max = s.Length;
+            }
+        }
+
+        Console.WriteLine(word);
+    }
+}
