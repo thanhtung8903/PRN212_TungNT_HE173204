c9ab13eeaeb93b92cbdbb4f9542730b96f1deaec
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 11:02:18 +0700

    Exercise 12

---
+++ Exercise12/Exercise12.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise12;
+
+public class Exercise12{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"{num} {num} {num} {num}");
+        Console.WriteLine($"{num}{num}{num}{num}");
+        Console.WriteLine($"{num} {num} {num} {num}");
+        Console.WriteLine($"{num}{num}{num}{num}");
+
+    }
+}
