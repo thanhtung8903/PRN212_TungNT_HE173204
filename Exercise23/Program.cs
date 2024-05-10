d6b6f41e41fd6b2a928ae23984ec6fd5033e7afd
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 11:53:22 +0700

    Exercise 23

---
+++ Exercise23/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise23;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        Console.WriteLine(str.ToLower());   
+    }
+}
