6ade1f0e5ac533d9e88aaf53afaf87b6dc1a3809
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 10:32:04 +0700

    Exercise 4

---
+++ Exercise4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(-1 + 4 * 6); 
+
+            System.Console.WriteLine((35 + 5) % 7); 
+
+            System.Console.WriteLine(14 + -4 * 6 / 11);
+
+            System.Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+        }
+    }
+}
