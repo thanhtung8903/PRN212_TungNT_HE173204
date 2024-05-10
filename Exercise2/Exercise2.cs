54a7ad9687d63f310d23ee1d6dc62ea79fbd1968
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 10:24:33 +0700

    Exercise 2

---
+++ Exercise2/Exercise2.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise2
+{
+    public class Exercise2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum of two numbers");
+
+            Console.WriteLine($"{15} + {17} = {15 + 17}");
+        }
+    }
+}
