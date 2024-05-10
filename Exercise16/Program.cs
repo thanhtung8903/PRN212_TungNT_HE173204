966c71cefa1b5b64b8c6fdcc47cbfeeb33992cdb
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 11:26:19 +0700

    Exercise 16

---
+++ Exercise16/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise16;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(first_last("w3resource"));
+        Console.WriteLine(first_last("Python"));
+        Console.WriteLine(first_last("x"));
+
+    }
+
+    public static string first_last(string str)
+    {
+        if (str.Length == 1)
+        {
+            return str;
+        }
+        else
+        {
+            return str.Substring(str.Length - 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1);
+        }
+    }
+}
