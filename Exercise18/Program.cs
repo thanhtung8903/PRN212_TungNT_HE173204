6bdfa3f8992140e7decec8a4de8546287a6b6146
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 11:31:08 +0700

    Exercise 18

---
+++ Exercise18/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise18;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input first integer:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input second integer:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        if ((num1 > 0 && num2 < 0) || (num1 < 0 && num2 > 0))
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+}
