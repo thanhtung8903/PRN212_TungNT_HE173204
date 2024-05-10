11d7d297666495604d110b8a434b6c2aec067bb2
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-10 10:57:53 +0700

    Exercise 11

---
+++ Exercise11/Exercise11.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise11;
+
+public class Exercise11
+{
+    public static void Main()
+    {
+        int age; 
+
+        Console.Write("Enter your age ");
+
+        age = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("You look younger than {0} ", age);
+    }
+}
