f698b19de9148b5b8438eb05c90bf447a2dc05cd
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 22:25:51 +0700

    Exercise 41

---
+++ Exercise 41/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise_41;
+
+public class Exercise41
+{
+    public static void Main( )
+    {
+        Console.Write("Input a string (contains at least one 'w' char) : ");
+
+        string str = Console.ReadLine();
+
+        var count = str.Count(s => s == 'w');
+
+        Console.WriteLine("Test the string contains 'w' character between 1 and 3 times: ");
+
+        Console.WriteLine(count >= 1 && count <= 3);   
+    }
+}
