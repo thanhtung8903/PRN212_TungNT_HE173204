b94650212853bc5743217a860421a22e8f4debec
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 21:55:36 +0700

    Exercise 33

---
+++ Exercise33/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercise33;
+
+public class Exercise33 {
+    static void Main(string[] args) {
+        Console.WriteLine("\nInput first integer:");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        if (x > 0) {
+            Console.WriteLine(x % 3 == 0 || x % 7 == 0);
+        }
+    }
+}
