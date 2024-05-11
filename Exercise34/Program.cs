c9578386ad8e66b0364a3021dad8a13903bf55d3
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 21:57:53 +0700

    Exercise 34

---
+++ Exercise34/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercise34;
+
+public class Exercise34 {
+    static void Main(string[] args) {
+        string str = Console.ReadLine();
+
+        Console.WriteLine((str.Length < 6 && str.Equals("Hello")) || (str.StartsWith("Hello") && str[5] == ' '));
+    }
+}
