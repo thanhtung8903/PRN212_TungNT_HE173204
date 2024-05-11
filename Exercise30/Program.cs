5a00335e4327f6eee223c5545f29bf2ab4a03a25
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 21:47:45 +0700

    Exercise 30

---
+++ Exercise30/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise30;
+
+public class Exercise30 {
+    public static void Main() {
+        string hexval = "4B0";
+
+        Console.WriteLine("Hexadecimal number: " + hexval);
+
+        int decValue = int.Parse(hexval, System.Globalization.NumberStyles.HexNumber);
+
+        Console.WriteLine("Convert to-");
+
+        Console.WriteLine("Decimal number: " + decValue);
+    }
+}
