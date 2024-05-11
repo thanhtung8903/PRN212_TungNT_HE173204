289ea240b0ec9fd1a6178f2d828aeffb76aa569a
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 22:37:48 +0700

    Exercise 44

---
+++ Exercise44/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise44;
+
+public class Exercise44 {
+    static void Main(string[] args) {
+        Console.Write("Input a string : ");
+
+        string str = Console.ReadLine();
+
+        var result = string.Empty;
+
+        for (var i = 0; i < str.Length; i++) {
+            if (i % 2 == 0)
+                result += str[i];
+        }
+        Console.WriteLine(result);
+    }
+}
