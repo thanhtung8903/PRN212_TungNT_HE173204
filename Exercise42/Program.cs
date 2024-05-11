7023615c542918cd5972fb7bde8a97c345a68c97
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 22:29:07 +0700

    Exercise 42

---
+++ Exercise42/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise42;
+
+public class Exercise42
+{
+    public static void Main( )
+    {
+        Console.Write("Input a string: ");
+
+        string str = Console.ReadLine();
+
+        if (str.Length < 4) 
+            Console.WriteLine(str.ToUpper());
+        else
+            Console.WriteLine(str.Substring(0, 4).ToLower() + str.Substring(4, str.Length - 4));
+    }
+}
