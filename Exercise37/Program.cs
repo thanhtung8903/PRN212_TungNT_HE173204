4fe20fe0a854fecaf5cbbfba83ce74f4fbddbc66
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 22:01:29 +0700

    Exercise 37

---
+++ Exercise37/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercise37;
+
+public class Exercise37 {
+    static void Main(string[] args) {
+        string str = "PHP Tutorial";
+
+        Console.WriteLine((str.Substring(1, 2).Equals("HP") ? str.Remove(1, 2) : str));
+    }
+}
