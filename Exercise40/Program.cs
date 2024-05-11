5f95953605a02271a24796b0b4ef52ab0d05a7f4
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 22:06:11 +0700

    Exercise 40

---
+++ Exercise40/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise40;
+
+public class Exercise40
+{
+    public static void Main( )
+    {
+        Console.WriteLine("\nInput first integer:");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Input second integer:");
+
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        int n = 20;
+
+        var val1 = Math.Abs(x - n);
+
+        var val2 = Math.Abs(y - n);
+
+        Console.WriteLine(val1 == val2 ? 0 : (val1 < val2 ? x : y));   
+    }
+}
