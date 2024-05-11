2ec9709fb04aac4c5931ed7ff6c10f3c2be964ce
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 21:52:54 +0700

    Exercise 32

---
+++ Exercise32/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise32;
+
+public class Exercise32 {
+    static void Main(string[] args) {
+        string str;
+
+        int l = 0;
+
+        Console.Write("Input a string : ");
+
+        str = Console.ReadLine();
+
+        if (str.Length > 4) {
+            Console.WriteLine(str.Length < 4 ? str + str + str : str.Substring(str.Length - 4) + str.Substring(str.Length - 4) + str.Substring(str.Length - 4) + str.Substring(str.Length - 4));
+        }
+    }
+}
