02be6b0ab3965cef6c60c4ed0349dbd40f994a33
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 22:40:54 +0700

    Exercise 46

---
+++ Exercise46/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise46;
+
+public class Exercise46
+{  
+    public static void Main() 
+    {
+        Console.WriteLine("\nInput an integer:");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        int[] nums = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 9};
+
+        Console.WriteLine((nums[0] == x) || (nums[nums.Length - 1] == x));
+    } 
+}
