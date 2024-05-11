69d7228a72fdc77749b0cc4ef9f407dd90e11776
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 22:32:40 +0700

    Exercise 43

---
+++ Exercise43/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise43;
+
+public class Exercise43 {
+    static void Main(string[] args) {
+        Console.Write("Input a string : ");
+
+        string str = Console.ReadLine();
+
+        Console.WriteLine(test(str));
+    }
+
+    public static bool test(string str) {
+        var ctr = 0; 
+
+        for (var i = 0; i < str.Length - 1; i++) {
+            if (str[i].Equals('w'))
+                ctr++;
+
+            if (str.Substring(i, 2).Equals("ww") && ctr > 2)
+                return true;
+        }
+
+        return false; 
+    }
+}
