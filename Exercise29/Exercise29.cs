9fc0ba85955e43fff1c8d6d7aff826fd1f546c75
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 21:46:35 +0700

    Exercise 29

---
+++ Exercise29/Exercise29.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise29;
+
+public class Exercise29 {
+    // This is the main method where the program execution starts
+    public static void Main() {
+        // Creating a FileInfo object representing a file at the specified path\
+        try
+        {
+            FileInfo f = new FileInfo("/home/students/abc.txt");
+
+            // Displaying the size of the file in bytes using FileInfo's Length property
+            Console.WriteLine("\nSize of a file: " + f.Length.ToString()); 
+        } catch (FileNotFoundException e)
+        {
+            Console.WriteLine("File not found");
+        }
+        
+        
+    }
+}
