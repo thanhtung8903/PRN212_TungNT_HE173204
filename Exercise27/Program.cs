126661f23f4668d2b8f352382c4f195ff7d96879
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 21:40:12 +0700

    Exercise 27

---
+++ Exercise27/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise27;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+        
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        
+        Console.WriteLine(sum);
+    }
+}
