cacbae47facb46f93d161fdb99eb0e0c8121fd57
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 22:03:55 +0700

    Exercise 39

---
+++ Exercise38/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise38;
+
+public class Exercise38 {
+    static void Main(string[] args) {
+        string str1 = "PHP Tutorial";
+
+        var result = "";
+
+        if (str1.Length >= 1 && str1[0] == 'P')
+            result += str1[0];
+
+        if (str1.Length >= 2 && str1[1] == 'H')
+            result += str1[1];
+
+        Console.WriteLine(result);
+    }
+}
---
+++ Exercise39/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise39;
+
+public class Exercise39 {
+    static void Main(string[] args) {
+        Console.WriteLine("\nInput first integer:");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Input second integer:");
+
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Input third integer:");
+
+        int z = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Largest of three: " + Math.Max(x, Math.Max(y, z)));
+
+        Console.WriteLine("Lowest of three: " + Math.Min(x, Math.Min(y, z)));
+    }
+}
