e270e0f898a388b2e84376b078767534867344d7
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 21:37:40 +0700

    Exercise 26

---
+++ Exercise26/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Channels;
+
+namespace Exercise26;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        int count = 0;
+        int i = 0;
+        while (count < 500)
+        {
+            if (IsPrime(i))
+            {
+                sum += i;
+                count++;
+            }
+
+            i++;
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    public static bool IsPrime(int n)
+    {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
