5b02fb17f765e1d2f45639fde5191e9c48c46c65
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 22:00:17 +0700

    Exercise 36

---
+++ Exercise36/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise36;
+
+public class Exercise36 {
+    static void Main(string[] args) {
+        Console.Write("Input a first number: ");
+
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input a second number: ");
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(((m >= -10 && m <= 10)) || ((n >= -10 && n <= 10)));
+    }
+}
