1beec988eeb3e0fad3b0058bfb919f4839cd063a
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 21:44:50 +0700

    Exercise 28

---
+++ Exercise28/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise28;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        
+        string[] words = str.Split(' ');
+    
+        string result = "";
+        
+        for (int i = words.Length - 1; i >= 0; i--)
+        {
+            result += words[i] + " ";
+        }
+        
+        Console.WriteLine(result);
+    }
+}
