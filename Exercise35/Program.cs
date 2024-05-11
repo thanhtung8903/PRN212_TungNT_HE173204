21016993455b03d6bbbb0afa59ee8e0ffac10da5
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-11 21:59:05 +0700

    Exercise 35

---
+++ Exercise35/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise35;
+
+public class Exercise35 {
+    static void Main(string[] args) {
+        Console.Write("Input a first number(<100): ");
+
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input a second number(>200): ");
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine((m < 100 && n > 200));
+    }
+}
