185a2e5f90931c91737be554664420e753f5bff8
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 00:53:03 +0700

    Exercise 65

---
+++ Exercise65/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise65;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 3, 5, 7, 9 };
+
+        int[] new_nums = test(nums);
+
+        Array.ForEach(new_nums, Console.WriteLine);
+    }
+
+    public static int[] test(int[] nums)
+    {
+        var arr_len = nums.Length;
+
+        return nums.Select(el => el * arr_len).ToArray();
+    }
+}
