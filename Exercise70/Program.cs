3d66c7070ed6a0a59db34da8acc026809eb1d658
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 00:59:21 +0700

    Exercise 70

---
+++ Exercise70/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise70;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Original string: PHP");
+        Console.WriteLine("After removing first and last elements: " + test("PHP"));
+
+        Console.WriteLine("Original string: Python");
+        Console.WriteLine("After removing first and last elements: " + test("Python"));
+
+        Console.WriteLine("Original string: JavaScript");
+        Console.WriteLine("After removing first and last elements: " + test("JavaScript"));
+    }
+
+    public static string test(string str1)
+    {
+        return str1.Length > 2 ? str1.Substring(1, str1.Length - 2) : str1;
+    }
+}
