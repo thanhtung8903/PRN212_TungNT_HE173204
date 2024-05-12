5a07804b853285e56a946c08b8365be83aeb4e1d
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-12 13:42:48 +0700

    Exercise 50

---
+++ Exercise50/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise50;
+
+public class Exercise50
+{  
+    public static void Main() 
+    {
+        int[] nums = {1, 2, 8};
+
+        Console.WriteLine("\nArray1: [{0}]", string.Join(", ", nums));
+
+        var temp = nums[0]; 
+
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            nums[i] = nums[i + 1]; 
+        }
+
+        nums[nums.Length - 1] = temp; 
+
+        Console.WriteLine("\nAfter rotating array becomes: [{0}]", string.Join(", ", nums));
+    } 
+}
