67f9b97cc21104aac349a70c2b26cfb22f776cd2
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 00:55:46 +0700

    Exercise 67

---
+++ Exercise67/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise67;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(test("PHP")); 
+        Console.WriteLine(test("JAVASCRIPT")); 
+    }
+
+    public static string test(string str1)
+    {
+        // Using multiple calls to Replace method to replace characters with specific digits
+        return str1.Replace("P", "9").Replace("T", "0").Replace("S", "1").Replace("H", "6").Replace("A", "8");
+    }
+}
