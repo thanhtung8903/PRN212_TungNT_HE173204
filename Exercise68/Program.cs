4a21fa7ed11a3b6893783f1bc797caf48a44ad03
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 00:57:15 +0700

    Exercise 68

---
+++ Exercise68/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise68;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(test("PHP Exercises", 'E', 'e')); // Output: 2
+        Console.WriteLine(test("Latest News, Breaking News LIVE", 'A', 'a')); // Output: 3
+    }
+
+    public static int test(string str1, char uc, char lc)
+    {
+        return str1.Split(uc, lc).Length - 1;
+    }
+}
