a5968333ac5b113bca0ea36d7f9bd7424bd59072
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 01:15:34 +0700

    Exercise 77

---
+++ Exercise77/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Is 'Exercise' plural? " + test("Exercise"));
+
+            Console.WriteLine("Is 'Exercises' plural? " + test("Exercises"));
+
+            Console.WriteLine("Is 'Books' plural? " + test("Books"));
+
+            Console.WriteLine("Is 'Book' plural? " + test("Book"));
+            Console.ReadLine();
+        }
+
+        public static bool test(string word)
+        {
+            return word.EndsWith("s");
+        }
+    }
+}
