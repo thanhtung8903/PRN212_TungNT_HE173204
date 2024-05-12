0ec8b1169468bf2acefc256490fe0256f70918ce
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-12 19:11:25 +0700

    Exercise 54

---
+++ Exercise54/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise54;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int year = Int32.Parse(Console.ReadLine());
+        Console.WriteLine(GetCentury(year));
+    }
+
+    public static int GetCentury(int year)
+    {
+        return (int)(year / 100) + ((year % 100 == 0) ? 0 : 1);
+    }
+} 
