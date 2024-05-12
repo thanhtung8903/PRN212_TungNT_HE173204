757d8a0061b6d4423c4504bf2001ada23f5ce9d0
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 00:58:16 +0700

    Exercise 69

---
+++ Exercise69/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise69;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(test("PHP")); // Output: True
+        Console.WriteLine(test("python")); // Output: True
+        Console.WriteLine(test("JavaScript")); // Output: False
+    }
+
+    public static bool test(string str1)
+    {
+        return str1 == str1.ToUpper() || str1 == str1.ToLower();
+    }
+}
