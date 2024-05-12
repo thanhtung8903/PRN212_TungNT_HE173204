cb584949543b6021441a945e8f5e407580a88746
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 01:00:06 +0700

    Exercise 71

---
+++ Exercise71/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercise71;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Original string: PHP");
+        Console.WriteLine("Test for consecutive similar letters! " + test("PHP"));
+
+        Console.WriteLine("Original string: PHHP");
+        Console.WriteLine("Test for consecutive similar letters! " + test("PHHP"));
+
+        Console.WriteLine("Original string: PHPP");
+        Console.WriteLine("Test for consecutive similar letters! " + test("PHPP"));
+
+        Console.WriteLine("Original string: PPHP");
+        Console.WriteLine("Test for consecutive similar letters! " + test("PPHP"));
+    }
+
+    public static bool test(string text)
+    {
+        for (int i = 0; i < text.Length - 1; i++)
+        {
+            if (text[i] == text[i + 1]) 
+            { 
+                return true; 
+            }
+        }
+
+        return false;
+    }
+}
