78952c23710f4990db355435622ddbc10301bbe4
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 01:13:21 +0700

    Exercise 76

---
+++ Exercise76/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise76
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ascii value of 1 is: " + test('1'));
+            Console.WriteLine("Ascii value of A is: " + test('A'));
+            Console.WriteLine("Ascii value of a is: " + test('a'));
+            Console.WriteLine("Ascii value of # is: " + test('#'));
+            Console.ReadLine();
+        }
+
+        public static int test(char ch)
+        {
+            return (int)ch;
+        }
+    }
+}
