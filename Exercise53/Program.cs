582d41d0eb36c2407f40a1f3be424682182265d6
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-12 19:03:54 +0700

    Exercise 53

---
+++ Exercise53/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise53;
+
+public class Exercise53
+{  
+    public static void Main() 
+    {
+        int[] nums = {2, 4, 7, 8, 6};
+
+        Console.WriteLine("\nOriginal array: [{0}]", string.Join(", ", nums));
+
+        Console.WriteLine("Check if an array contains an odd number? " + even_odd(nums));
+    }  
+
+    public static bool even_odd(int[] nums)  
+    {
+        foreach (var n in nums)
+        {
+            if (n % 2 != 0) 
+                return true; 
+        }
+        return false;
+    } 
+}
