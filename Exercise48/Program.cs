87ed3bb392db502edaa60c81fbab07133173c4e1
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-12 13:38:12 +0700

    Exercise 48

---
+++ Exercise48/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise48;
+
+public class Exercise48
+{  
+    public static void Main() 
+    {
+        int[] nums = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1};
+
+        Console.WriteLine((nums.Length >= 1) && (nums[0].Equals(nums[nums.Length - 1])));
+    } 
+}
