4ed82997b342758af15a1feb318df34c3230bdbe
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-12 19:02:30 +0700

    Exercise 52

---
+++ Exercise52/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise52;
+
+public class Exercise52
+{  
+    public static void Main() 
+    {
+        int[] array1 = {1, 2, 5};
+        Console.WriteLine("\nArray1: [{0}]", string.Join(", ", array1));
+        int[] array2 = {0, 3, 8};
+        Console.WriteLine("\nArray2: [{0}]", string.Join(", ", array2));
+        int[] array3 = {-1, 0, 2};
+        Console.WriteLine("\nArray3: [{0}]", string.Join(", ", array3));
+
+        int[] new_array = { array1[1], array2[1], array3[1] };
+        Console.WriteLine("\nNew array: [{0}]", string.Join(", ", new_array));
+    } 
+}
