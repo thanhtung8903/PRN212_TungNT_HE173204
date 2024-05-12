233345f613600896deb71f5f0e27958615728091
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 00:54:19 +0700

    Exercise 66

---
+++ Exercise66/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise66;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(test("12", "43"));
+    }
+
+    public static string test(string strn1, string strn2)
+    {
+        return Int32.Parse(strn1) > Int32.Parse(strn2) ? strn2 : strn1;
+    }
+}
