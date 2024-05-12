d32c484a3451375bbea17796dd9590465cb63b84
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 01:04:34 +0700

    Exercise 74

---
+++ Exercise74/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise74;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Original string: PHP");
+        Console.WriteLine("Length of the string: " + test("PHP"));
+
+        Console.WriteLine("Original string: javascript");
+        Console.WriteLine("Length of the string: " + test("javascript"));
+
+        Console.WriteLine("Original string: python");
+        Console.WriteLine("Length of the string: " + test("python"));
+    }
+
+    public static string test(string word)
+    {
+        int length = word.Length; 
+
+        if (length % 2 == 0) 
+        {
+            return "Even length";
+        }
+        else 
+        {
+            return "Odd length";
+        }
+    }
+}
