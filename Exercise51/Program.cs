2043f73ff0b75f77e28167437534feb4405447d2
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-12 18:59:36 +0700

    Exercise 51

---
+++ Exercise51/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise51;
+
+public class Exercise51
+{  
+    public static void Main() 
+    {
+        int[] nums = {1, 2, 5, 7, 8};
+
+        Console.WriteLine("\nArray1: [{0}]", string.Join(", ", nums));
+
+        var h_val = nums[0];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[0])
+                h_val = nums[i]; 
+        }
+
+        Console.WriteLine("\nHighest value between first and last values of the said array: {0}", h_val);
+    } 
+}
