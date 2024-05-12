1cf456bfaaa030be121caae95199e87965ffcd46
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 00:51:21 +0700

    Exercise 64

---
+++ Exercise64/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise64;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string file_path;
+
+        file_path = "c:/csharp/ex/test.cpp";
+        Console.WriteLine(test(file_path));  
+
+        file_path = "c:/movies/abc.mp4";
+        Console.WriteLine(test(file_path));  
+
+        file_path = "test.txt";
+        Console.WriteLine(test(file_path));  
+    }
+
+    public static string test(string file_path)
+    {
+        return file_path.Split('/').Last();
+    }
+}
