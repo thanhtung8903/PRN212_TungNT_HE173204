5e989b42b90f6d3753d07ec66aafdfa793a75698
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 01:03:48 +0700

    Exercise 73

---
+++ Exercise73/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise73;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Original string: PHP");
+        Console.WriteLine("Convert the letters of the said string into alphabetical order: " + test("PHP"));
+
+        Console.WriteLine("Original string: javascript");
+        Console.WriteLine("Convert the letters of the said string into alphabetical order: " + test("javascript"));
+
+        Console.WriteLine("Original string: python");
+        Console.WriteLine("Convert the letters of the said string into alphabetical order: " + test("python"));
+    }
+
+    public static string test(string str1)
+    {
+        return new string(str1.OrderBy(x => x).ToArray());
+    }
+}
