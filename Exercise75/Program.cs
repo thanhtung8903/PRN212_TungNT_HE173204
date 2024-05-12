e783e88616776263f1434336764d33e2929b9ad1
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 01:12:57 +0700

    Exercise 75

---
+++ Exercise75/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1st odd number: " + test(1));
+            Console.WriteLine("2nd odd number: " + test(2));
+            Console.WriteLine("4th odd number: " + test(4));
+            Console.WriteLine("100th odd number: " + test(100));
+            Console.ReadLine();
+        }
+
+        public static int test(int n)
+        {
+            return n * 2 - 1; 
+        }
+    }
+}
