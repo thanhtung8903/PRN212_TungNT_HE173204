19f070d5ac1ff3e5bc607e6a9e2d71c1ed86d55e
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-12 19:21:37 +0700

    Exercise 56

---
+++ Exercise56/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise56;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        
+        string result = "";
+        
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            result += str[i];
+        }
+
+        if (str.Equals(result))
+        {
+            Console.WriteLine(true);
+        } 
+        else
+        {
+            Console.WriteLine(false);
+        }
+    }
+}
