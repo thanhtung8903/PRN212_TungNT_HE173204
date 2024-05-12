cba496acca311b9dea2755a7daa25d19472d0eaa
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 00:49:15 +0700

    Exercise 63

---
+++ Exercise63/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise63;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 3, 5, 7, 9 };
+        int n = 6;
+
+        Console.WriteLine(test(nums, n));
+
+        n = 3;
+
+        Console.WriteLine(test(nums, n));
+    }
+
+    public static bool test(int[] arra, int n)
+    {
+        return arra.Contains(n);
+    }
+}
