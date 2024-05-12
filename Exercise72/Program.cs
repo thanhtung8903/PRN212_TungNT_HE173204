bb9dffd075039dff346e5fcf05eb9ba4e615d5bf
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 01:01:49 +0700

    Exercise 72

---
+++ Exercise72/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise72;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 5, 4, 2, 3, 4 };
+        Console.WriteLine("Check the average value of the said array is a whole number or not: " + test(nums));
+
+        int[] nums1 = { 2, 4, 2, 6, 4, 8 };
+        Console.WriteLine("Check the average value of the said array is a whole number or not: " + test(nums1));
+    }
+
+    public static bool test(int[] arr_nums)
+    {
+        return arr_nums.Average() % 1 == 0;
+    }
+}
