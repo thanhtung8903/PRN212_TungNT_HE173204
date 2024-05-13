c5575958b2a59da9e289f89b18a955a369efa76c
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-13 10:57:55 +0700

    Exercise 78

---
+++ Exercise78/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise78
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3 };
+
+            Console.WriteLine("Sum of squares of elements of the said array: " + test(nums));
+
+            int[] nums1 = { -2, 0, 3, 4 };
+
+            Console.WriteLine("Sum of squares of elements of the said array: " + test(nums1));
+            Console.ReadLine();
+        }
+
+        public static int test(int[] nums)
+        {
+            return nums.Sum(n => n * n);
+        }
+    }
+}
